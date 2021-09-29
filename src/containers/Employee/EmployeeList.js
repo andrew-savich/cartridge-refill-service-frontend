04d9c6fc0c45c81169273196efd8efecd337dd31
andrew-savich <savichandrew@Gmail.com>
2021-09-29 11:05:48 +0300

    EmployeeList to functional comp with hooks

--- src/containers/Employee/EmployeeList.js
+++ src/containers/Employee/EmployeeList.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import EmployeeService from '../../services/EmployeeService';
 import { Table } from '../../components/Table/Table';
 import { Button } from '../../components/Button/Button';
 
-export default class EmployeeList extends Component {
-    constructor(props){
-        super(props);
+ const EmployeeList = props => {
+    const [employees, setEmployees] = useState([]);
 
-        this.state = {
-            employees: []
-        }
-       
-        this.addEmployeeHandler = this.addEmployeeHandler.bind(this);
+    useEffect(()=>{
+        const init = async () => {
+            const response = await EmployeeService.getEmployees();
+            setEmployees(response.data);
+        };
+
+        init();
+    }, []);
+
+
+    const addEmployeeHandler = () => {
+       props.history.push('/add-employee');
     }
 
-    async componentDidMount() {
-        const respponse = await EmployeeService.getEmployees();
-        this.setState({
-            employees: respponse.data
-        })
-        
-    }
+   
+    return (
+        <div className="container">
+            <h2 className="text-center">Employees</h2>
 
-    addEmployeeHandler(){
-       this.props.history.push('/add-employee');
-    }
+            <Button className="btn btn-primary" onClick={addEmployeeHandler} title="Add Employee" />
 
-    render() {
-        return (
-            <div className="container">
-                <h2 className="text-center">Employees</h2>
-
-                <Button className="btn btn-primary" onClick={this.addEmployeeHandler} title="Add Employee" />
-
-                <div className="row">
-                    {
-                     this.state.employees.length !== 0 ? <Table entities={this.state.employees}/> : <h3>Empty</h3>
-                    }
-                </div>
-
+            <div className="row">
+                {
+                    employees.length !== 0 ? <Table entities={employees}/> : <h3>Empty</h3>
+                }
             </div>
-        )
-    }
+
+        </div>
+    )
+    
 }
+
+export default EmployeeList;
